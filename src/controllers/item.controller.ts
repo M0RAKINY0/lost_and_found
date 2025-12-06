28f9388111400bf40d65beecd07f0e86c7bdd998
morakinyo <morakinyokolade43@gmail.com>
2025-12-06 20:43:13 +0100

    updated and fixed prisma errors

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import prisma from "../prisma";
 
 export const createItem = async (req: Request, res: Response) => {
     const { name, description } = req.body;
@@ -11,15 +11,25 @@ export const createItem = async (req: Request, res: Response) => {
     });
     res.status(201).json(item);
 }
-export const updateItem = async ()
 export const itemList = async (req: Request, res: Response) => {
-    const {item, description} = req.body
-    const itemList = await prisma.findMany({
+    const { name, description } = req.body;
+    const items = await prisma.item.findMany({
         where: {
-            data:{ 
-                item: item,
-                description: description
-            }
-        }
+            name,
+            description,
+        },
     });
+    res.json(items);
 }
+
+export const updateItem = async (req: Request, res: Response) => {
+    const { id, name, description } = req.body;
+    const item = await prisma.item.update({
+        where: { id },
+        data: {
+            name,
+            description,
+        },
+    });
+    res.json(item);
+};
--- src/prisma.config.ts
+++ src/prisma.config.ts
@@ -9,9 +9,8 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
+
   datasource: {
     url: env("DATABASE_URL"),
   },
 });
-
